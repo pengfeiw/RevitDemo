ca4a011c98fb183afe513ccc4bf83bc4c9ce758a
Pengfei Wang <13671619364@sina.cn>
2019-05-06 15:20:41 +0800

    Add filtering project

---
+++ src/Filtering/Filtering.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Filtering
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Filtering : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document document = uidoc.Document;
+            ElementCategoryFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
+            FilteredElementCollector collector = new FilteredElementCollector(document);
+
+            //use WhereElementIsNotElementType() to find all wall instances only(not include wall type).
+            IList<Element> walls = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements();
+
+            string prompt = "The walls in the current document are:\n";
+            foreach (Element e in walls)
+            {
+                prompt += e.Name + "\n";
+            }
+
+            TaskDialog.Show("WPF", prompt);
+
+            return Result.Succeeded;
+        }
+    }
+}
